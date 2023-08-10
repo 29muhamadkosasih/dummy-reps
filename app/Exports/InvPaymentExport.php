28c193b6f16fd64ef2a0c174ce96cd81e897f51c
29muhamadkosasih <muhamadkosasih28oct@gmail.com>
2023-08-10 09:29:22 +0700

    0989

--- app/Exports/InvPaymentExport.php
+++ app/Exports/InvPaymentExport.php
@@ -33,16 +33,16 @@ class InvPaymentExport implements FromCollection, WithHeadings
     public function headings(): array
     {
         return [
-            'client',
-            'no_invoice',
-            'bruto',
-            'payment_in',
-            'paid_date',
-            'pph23',
-            'npwp',
-            'masa_pajak',
-            'no_bukpot',
-            'tgl_pemotongan',
+            'CLIENT',
+            'INV NO',
+            'BRUTO',
+            'PAYMENT IN',
+            'PAID DATE',
+            'PPH 23',
+            'NPWP',
+            'MASA PAJAK',
+            'NO BUKPOT',
+            'TGL PEMOTONGAN',
         ];
     }
 }
